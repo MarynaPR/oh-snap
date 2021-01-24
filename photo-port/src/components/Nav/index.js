65763c418a0621a99a08b31f5e20ab6027b05a8e
MarynaPR <pryadkamaryna@gmail.com>
2021-01-24 17:30:14 -0600

    about and nav tests passed

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -14,16 +14,16 @@ function Nav() {
   }
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
               About me
             </a>
           </li>
@@ -35,11 +35,11 @@ function Nav() {
           {
             categories.map((category) => (
               <li className="mx-1" key={category.name} >
-                <span onClick={ handleClick }>
-                  { capitalizeFirstLetter(category.name)}
+                <span onClick={handleClick}>
+                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
-                
+
             ))
           }
         </ul>
