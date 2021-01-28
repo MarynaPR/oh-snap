a8f1ae2143a691585f0de1e0794e38332a70cfb3
MarynaPR <pryadkamaryna@gmail.com>
2021-01-27 18:50:39 -0600

    link for pictures fixed

--- photo-port/src/components/PhotoList/index.js
+++ photo-port/src/components/PhotoList/index.js
@@ -3,7 +3,6 @@ import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const [currentPhoto, setCurrentPhoto] = useState();
   const [photos] = useState([
     {
@@ -111,20 +110,16 @@ const PhotoList = ({ category }) => {
   }
   return (
     <div>
-      {isModalOpen && (
-        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
-      )}
-
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
             key={image.name}
           />
-
         ))}
       </div>
     </div>
